482b4337236cfb57cb4c2724738398bb8c5469a9
lynxl <lynxleuck@gmail.com>
2021-04-18 23:03:43 -0400

    getCustomersAccounts test done; removeCustomer test done; addCustomerandAddtoList test done;

---
+++ src/test/java/bankingTest/BankTest.java
@@ -0,0 +1,54 @@
+
+package bankingTest;
+import java.lang.*;
+import banking.Bank;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.*;
+public class BankTest {
+    Bank bank;
+
+    @Test
+    void main() {
+
+    }
+
+    @Test
+    public void getAllAccounts(){
+
+    }
+
+    @Test
+    void addCustomerWizard() {
+    }
+
+    @Test
+    void addCustomerandAddtoList() {
+        final String firstName ="Lynx", lastName = "Leuck";
+        bank.addCustomer(firstName, lastName);
+        assertNotNull("If there are names this is not null: ", bank.getCustomer(firstName, lastName));
+    }
+
+    @Test
+    void removeCustomer() {
+        final String customerId = "1";
+        bank.removeCustomer(customerId);
+        assertNull("",bank.getCustomer(customerId));
+    }
+
+    @Test
+    void getAllCustomers() {
+    }
+
+    @Test
+    void testGetCustomer() {
+    }
+
+    @Test
+    void getCustomersAccounts() {
+        final String customerId = "1";
+        assertNotNull(bank.getCustomersAccounts(customerId));
+    }
+}
