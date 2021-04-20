c5974ae291f1ad7221994ef381ffacf8da3fbb44
lynxl <lynxleuck@gmail.com>
2021-04-19 21:24:02 -0400

    Added notes to AccountTest

--- src/test/java/bankingTest/AccountTest.java
+++ src/test/java/bankingTest/AccountTest.java
@@ -10,13 +10,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class AccountTest {
+    //Sets up static variable for the first account
     double startingAccountAmount = 100.00;
     String accountDescription = "Rainy Day Fund";
-    String accountTwoDescription = "Flight to the Moon";
     String customerFirstName = "Tim";
     String customerLastName = "Johnson";
     Bank theBigBank = new Bank("The Big Bank");
+
+    //This is the one variable that needs to change for the account
+    String accountTwoDescription = "Flight to the Moon";
+
+    //Sets up the customer using the variables above
     Customer tim = new Customer(theBigBank, customerFirstName, customerLastName);
+
+    //Sets up the first account and implements a few required methods
     Account account = new Account(tim, startingAccountAmount, accountDescription) {
         public void deposit(double amount) {
             //Stub
@@ -26,6 +33,8 @@ class AccountTest {
             //Stub
         }
     };
+
+    //Sets up the second account for testing against
     Account accounttwo = new Account(tim, startingAccountAmount, accountTwoDescription) {
         public void deposit(double amount) {
             //Stub
@@ -38,15 +47,20 @@ class AccountTest {
 
     @Test
     void testDeposit() {
+        //sets the static variables
         double amountToDeposit = 100.00;
         double accountStartingAmount = account.getBalance();
+        //attempts to deposit
         account.deposit(amountToDeposit);
+
+        //Checks the balance amount and compares it to the original amount
         assertTrue(account.getBalance() == accountStartingAmount + amountToDeposit, "Deposit Test Results");
     }
 
 
     @Test
     void transfer() {
+        //Checks and sets the initial variables to be compared later
         double startingAmount = accounttwo.getBalance();
         double transferAmount = 100.00;
         //Ensures there is a starting amount
