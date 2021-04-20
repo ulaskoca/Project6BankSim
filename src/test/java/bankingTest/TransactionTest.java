15c39b7119e7705af6697df4b074e1a8a33cc9fc
lynxl <lynxleuck@gmail.com>
2021-04-19 21:44:44 -0400

    Opened and Started TransactionTest

---
+++ src/test/java/bankingTest/TransactionTest.java
@@ -0,0 +1,17 @@
+package bankingTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionTest {
+
+    @Test
+    void testEquals() {
+
+    }
+
+    @Test
+    void compareTo() {
+    }
+}
